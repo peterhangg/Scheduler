097fae19a040f258021ccfadfae269d98b5dc630
Peter Hang <peterhanggg@gmail.com>
2019-10-28 13:25:01 -0400

    completed selecting days activity by adding useState

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,14 +2,16 @@ import React from 'react'
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  const days = props.days.map((day, index) => 
-    <DayListItem 
-      key={day.id}
-      name={day.name} 
-      spots={day.spots} 
-      selected={day.name === props.day}
-      setDay={props.setDay}  
-    />
+  const days = props.days.map((day, index) => {
+    return ( 
+      <DayListItem 
+        key={day.id}
+        name={day.name} 
+        spots={day.spots} 
+        selected={day.name === props.day}
+        setDay={props.setDay}  
+      />
+    )}
   )
   
   return (
