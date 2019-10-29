45cc1a3bd200aa29eb031715cde63b64deaef81a
Peter Hang <peterhanggg@gmail.com>
2019-10-29 17:15:50 -0400

    created appointment helper function and testing

---
+++ src/helpers/selectors.js
@@ -0,0 +1,12 @@
+export function getAppointmentsForDay(state, day) {
+  const result = [];
+  const filteredDay = state.days.filter(days => days.name === day);
+  if(filteredDay.length > 0) {
+    filteredDay[0].appointments.forEach(appt => {
+      if(state.appointments[appt]) {
+        result.push(state.appointments[appt])
+      }
+    });
+  }
+  return result;
+}
