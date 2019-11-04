cedd2880cfe569f1676d0dffd47e5458667f9545
Peter Hang <peterhanggg@gmail.com>
2019-11-04 16:15:36 -0500

    added propTypes and completed 'our test first' assignment by adding testing to form

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react"; // the render function allows us to render Components
 
-import Application from "components/Application";
+import Application from "components/Application"; // import components that we are testing
 
 afterEach(cleanup);
 
